943650d9785e8ebfcc4044a415001d73f1534124
tanglx61 <tanglx61@gmail.com>
2016-02-28 20:24:12 -0500

    calibrated data generation ratio

--- app/config.js
+++ app/config.js
@@ -1,14 +1,14 @@
 var MULTIPLIER = 10;
 
-//try to keep the ratio, change the MULTIPLIER value instead for bigger dataset
+//try to keep the ratio (they're carefully calibrated), change the MULTIPLIER value instead for bigger dataset
 var config = {
 	users: 1 * MULTIPLIER,
-	posts: 3 * MULTIPLIER,
-	comments: 10 * MULTIPLIER,
+	posts: 5 * MULTIPLIER,
+	comments: 30 * MULTIPLIER,
 	notifications: 2 * MULTIPLIER,
 	votes: {
-		posts: 2 * MULTIPLIER,
-		comments: 5 * MULTIPLIER,
+		posts: 4 * MULTIPLIER,
+		comments: 6 * MULTIPLIER,
 	},
 	events: {
 		browsing: 20 * MULTIPLIER,
